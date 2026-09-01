8382b47e92bc9d532cc1620c3260f52940c4cf06
sami-ramadan <samiramadan389@gmail.com>
2026-09-01 21:37:40 +0300

    Add PageBanner component and About Us page

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,13 @@
+import PageBanner from "@/components/reusable/page-banner";
+
+export default function Page() {
+  return (
+    <div>
+      <PageBanner
+        title="About Us"
+        subtitle="Guiding Hearts, Enlightening Minds – Learn Quran Online with Tareeq Al Jannah"
+        imageSrc="/about-banner.png"
+      />
+    </div>
+  );
+}
---
+++ src/components/reusable/page-banner.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+export default function PageBanner({
+  title,
+  subtitle,
+  imageSrc,
+}: {
+  title: string;
+  subtitle: string;
+  imageSrc: string;
+}) {
+  return (
+    <div className="h-75 relative">
+      <div className="absolute inset-0 bg-black opacity-50" />
+
+      <Image
+        src={imageSrc}
+        alt="Page Banner"
+        width={1200}
+        height={300}
+        className="w-full h-full object-cover"
+      />
+
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-white text-center">
+        <h1 className="text-3xl font-bold">{title}</h1>
+        <p className="mt-2 text-lg">{subtitle}</p>
+      </div>
+    </div>
+  );
+}
